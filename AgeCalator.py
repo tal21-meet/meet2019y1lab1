ff4db1d197afa1ea863ac64cb1675f537f87c7e0
tal21-meet <tal21@meet.mit.edu>
2019-07-23 03:16:07 -0400

    Stopping here

---
+++ AgeCalator.py
@@ -0,0 +1,8 @@
+person_one = input("What is the age of the first person? ")
+person_two = input("What is the age of the secend person? ")
+one_num=int(person_one)
+two_num=int(person_two)
+age_Difference = two_num-one_num
+age_difference = abs(age_Difference)
+str(age_difference)
+print(age_difference)
